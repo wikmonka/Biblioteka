c9f88e15885a768a5ea1c3461e73a1bc66bbf568
Wiktor Mateusz Mońka <witek9965@gmail.com>
2025-09-18 13:08:20 +0200

    Filtrowanie dla Bibliotekarza

--- Bibliotekarz.cpp
+++ Bibliotekarz.cpp
@@ -55,47 +55,122 @@ void Bibliotekarz::menu() {
 }
 
 void Bibliotekarz::wyswietlKsiazki() {
-	std::ifstream plik("ksiazki.txt", std::ios::in);
-	std::string linia;
-	std::cout << "\n";
-	if (plik.good() == false)
-	{
-		std::cerr << "Nie mozna otworzyc pliku!" << std::endl;
-		exit(0);
-	}
-	else {
-		std::cout << "Lista ksiazek w bibliotece: " << std::endl;
-		while (std::getline(plik, linia)) {
-			std::string tytul, autor_imie, autor_nazwisko, gatunek, opis, s_rok, nosnik, stan;
-			std::istringstream iss(linia);
-			std::getline(iss, tytul, ';');
-			std::getline(iss, autor_imie, ';');
-			std::getline(iss, autor_nazwisko, ';');
-			std::getline(iss, gatunek, ';');
-			std::getline(iss, opis, ';');
-			std::getline(iss, s_rok, ';');
-			std::getline(iss, nosnik, ';');
-			std::getline(iss, stan, ';');
+    std::ifstream plik("ksiazki.txt", std::ios::in);
+    std::string linia, wybor, szukany_autor, szukany_nosnik, szukany_gatunek, szukany_czytelnik;
 
-			int rok_wydania = std::stoi(s_rok);
+    std::cout << "\n";
+    std::cout << "Wybierz typ filtra" << std::endl;
+    std::cout << "1. Wszystko" << std::endl;
+    std::cout << "2. Dostepne" << std::endl;
+    std::cout << "3. Po nazwisku Autora" << std::endl;
+    std::cout << "4. Po gatunku" << std::endl;
+    std::cout << "5. Po nosniku" << std::endl;
+    std::cout << "6. Wypozyczone przez konkretnego Czytelnika" << std::endl;
+    std::cout << "Wybor: ";
+    std::cin >> wybor;
+    std::cout << "\n";
 
-			std::cout << "Tytul: " << tytul << std::endl;
-			std::cout << "Autor: " << autor_imie << " " << autor_nazwisko << std::endl;
-			std::cout << "Gatunek: " << gatunek << std::endl;
-			std::cout << "Opis: " << opis << std::endl;
-			std::cout << "Rok wydania: " << rok_wydania << std::endl;
-			std::cout << "Nosnik: " << nosnik << std::endl;
-			if (stan == "wolna") {
-				std::cout << "Stan: Dostepna" << std::endl;
-			}
-			else
-			{
-				std::cout << "Stan: Wypozyczona przez - " << stan << std::endl;
-			}
-			std::cout << "--------------------------------" << std::endl;
-		}
-	}
+    if (wybor == "3") {
+        std::cout << "Podaj nazwisko autora: ";
+        std::cin >> szukany_autor;
+    }
+    else if (wybor == "4") {
+        std::cout << "Dostepne gatunki: Horror/Dramat/Komedia/Sci-Fi/Beletrystyka/Dokument/Biografia/Kryminal/Thriller/Romans. " << std::endl;
+        std::cout << "Podaj gatunek: ";
+        std::cin >> szukany_gatunek;
+    }
+    else if (wybor == "5") {
+        std::cout << "Podaj nosnik (ksiazka/e-book/audiobook): ";
+        std::cin >> szukany_nosnik;
+    }
+    else if (wybor == "6") {
+        std::cout << "Podaj login Czytelnika:  ";
+        std::cin >> szukany_czytelnik;
+    }
 
+
+    if (plik.good() == false)
+    {
+        std::cerr << "Nie mozna otworzyc pliku!" << std::endl;
+        exit(0);
+    }
+    else {
+        std::cout << "Lista ksiazek w bibliotece: " << std::endl;
+        std::cout << "\n";
+        bool found = false;
+        while (std::getline(plik, linia)) {
+            std::string tytul, autor_imie, autor_nazwisko, gatunek, opis, s_rok, nosnik, stan;
+
+            std::istringstream iss(linia);
+            std::getline(iss, tytul, ';');
+            std::getline(iss, autor_imie, ';');
+            std::getline(iss, autor_nazwisko, ';');
+            std::getline(iss, gatunek, ';');
+            std::getline(iss, opis, ';');
+            std::getline(iss, s_rok, ';');
+            std::getline(iss, nosnik, ';');
+            std::getline(iss, stan, ';');
+
+            int rok_wydania = std::stoi(s_rok);
+            bool match = false;
+
+            if (wybor == "1") {
+                match = true;
+            }
+            else if (wybor == "2") {
+                if (stan == "wolna") {
+                    match = true;
+                }
+            }
+            else if (wybor == "3") {
+                if (autor_nazwisko == szukany_autor) {
+                    match = true;
+                }
+            }
+            else if (wybor == "4") {
+                if (gatunek == szukany_gatunek) {
+                    match = true;
+                }
+            }
+            else if (wybor == "5") {
+                if (nosnik == szukany_nosnik) {
+                    match = true;
+                }
+            }
+            else if (wybor == "6") {
+                if (stan == szukany_czytelnik) {
+                    match = true;
+                }
+            }
+            else {
+                std::cout << "Wybierz dostepna opcje!" << std::endl;
+                std::cout << "\n";
+                break;
+            }
+
+            if (match) {
+                std::cout << "Tytul: " << tytul << std::endl;
+                std::cout << "Autor: " << autor_imie << " " << autor_nazwisko << std::endl;
+                std::cout << "Gatunek: " << gatunek << std::endl;
+                std::cout << "Opis: " << opis << std::endl;
+                std::cout << "Rok wydania: " << rok_wydania << std::endl;
+                std::cout << "Nosnik: " << nosnik << std::endl;
+                if (stan == "wolna") {
+                    std::cout << "Stan: Dostepna" << std::endl;
+                }
+                else {
+                    std::cout << "Stan: Wypozyczona przez - " << stan << std::endl;
+                }
+                std::cout << "--------------------------------" << std::endl;
+                found = true;
+            }
+        }
+        if (!found) {
+            std::cout << "Nie znaleziono pasujacych ksiazek." << std::endl;
+        }
+    }
+    plik.close();
+    std::cout << "\n";
 }
 
 
